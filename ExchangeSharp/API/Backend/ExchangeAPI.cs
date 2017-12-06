af3d407fc2b1b59461389190d712b2bd0bb24201
Jeff Johnson <jjxtra@gmail.com>
2017-12-06 13:16:10 -0700

    Add async implementations of API endpoints

--- ExchangeSharp/API/Backend/ExchangeAPI.cs
+++ ExchangeSharp/API/Backend/ExchangeAPI.cs
@@ -48,6 +48,11 @@ namespace ExchangeSharp
         public abstract string BaseUrl { get; set; }
 
         /// <summary>
+        /// Gets the name of the exchange
+        /// </summary>
+        public abstract string Name { get; }
+
+        /// <summary>
         /// Public API key - only needs to be set if you are using private authenticated end points. Please use CryptoUtility.SaveUnprotectedStringsToFile to store your API keys, never store them in plain text!
         /// </summary>
         public System.Security.SecureString PublicApiKey { get; set; }
@@ -94,6 +99,19 @@ namespace ExchangeSharp
         public System.Net.Cache.RequestCachePolicy CachePolicy { get; set; } = new System.Net.Cache.RequestCachePolicy(System.Net.Cache.RequestCacheLevel.NoCacheNoStore);
 
         private readonly Dictionary<string, KeyValuePair<DateTime, object>> cache = new Dictionary<string, KeyValuePair<DateTime, object>>();
+        private static readonly Dictionary<string, IExchangeAPI> apis = new Dictionary<string, IExchangeAPI>(StringComparer.OrdinalIgnoreCase);
+
+        /// <summary>
+        /// Static constructor
+        /// </summary>
+        static ExchangeAPI()
+        {
+            foreach (Type type in typeof(ExchangeAPI).Assembly.GetTypes().Where(type => type.IsSubclassOf(typeof(ExchangeAPI))))
+            {
+                ExchangeAPI api = Activator.CreateInstance(type) as ExchangeAPI;
+                apis[api.Name] = api;
+            }
+        }
 
         /// <summary>
         /// Process a request url
@@ -167,6 +185,75 @@ namespace ExchangeSharp
             return form;
         }
 
+        protected bool ReadCache<T>(string key, out T value)
+        {
+            lock (cache)
+            {
+                if (cache.TryGetValue(key, out KeyValuePair<DateTime, object> cacheValue))
+                {
+                    // if not expired, return
+                    if (cacheValue.Key > DateTime.UtcNow)
+                    {
+                        value = (T)cacheValue.Value;
+                        return true;
+                    }
+                    cache.Remove(key);
+                }
+            }
+            value = default(T);
+            return false;
+        }
+
+        protected void WriteCache<T>(string key, TimeSpan expiration, T value)
+        {
+            lock (cache)
+            {
+                cache[key] = new KeyValuePair<DateTime, object>(DateTime.UtcNow + expiration, value);
+            }
+        }
+
+        /// <summary>
+        /// Get an exchange API given an exchange name (see public constants at top of this file)
+        /// </summary>
+        /// <param name="exchangeName">Exchange name</param>
+        /// <returns>Exchange API or null if not found</returns>
+        public static IExchangeAPI GetExchangeAPI(string exchangeName)
+        {
+            GetExchangeAPIDictionary().TryGetValue(exchangeName, out IExchangeAPI api);
+            return api;
+        }
+
+        /// <summary>
+        /// Get a dictionary of exchange APIs for all exchanges
+        /// </summary>
+        /// <returns>Dictionary of string exchange name and value exchange api</returns>
+        public static IReadOnlyDictionary<string, IExchangeAPI> GetExchangeAPIDictionary()
+        {
+            return apis;
+        }
+
+        /// <summary>
+        /// Load API keys from an encrypted file - keys will stay encrypted in memory
+        /// </summary>
+        /// <param name="encryptedFile">Encrypted file to load keys from</param>
+        public virtual void LoadAPIKeys(string encryptedFile)
+        {
+            SecureString[] strings = CryptoUtility.LoadProtectedStringsFromFile(encryptedFile);
+            if (strings.Length != 2)
+            {
+                throw new InvalidOperationException("Encrypted keys file should have a public and private key");
+            }
+            PublicApiKey = strings[0];
+            PrivateApiKey = strings[1];
+        }
+
+        /// <summary>
+        /// Normalize a symbol for use on this exchange
+        /// </summary>
+        /// <param name="symbol">Symbol</param>
+        /// <returns>Normalized symbol</returns>
+        public virtual string NormalizeSymbol(string symbol) { return symbol; }
+
         /// <summary>
         /// Make a request to a path on the API
         /// </summary>
@@ -229,32 +316,16 @@ namespace ExchangeSharp
             return responseString;
         }
 
-        protected bool ReadCache<T>(string key, out T value)
-        {
-            lock (cache)
-            {
-                if (cache.TryGetValue(key, out KeyValuePair<DateTime, object> cacheValue))
-                {
-                    // if not expired, return
-                    if (cacheValue.Key > DateTime.UtcNow)
-                    {
-                        value = (T)cacheValue.Value;
-                        return true;
-                    }
-                    cache.Remove(key);
-                }
-            }
-            value = default(T);
-            return false;
-        }
-
-        protected void WriteCache<T>(string key, TimeSpan expiration, T value)
-        {
-            lock (cache)
-            {
-                cache[key] = new KeyValuePair<DateTime, object>(DateTime.UtcNow + expiration, value);
-            }
-        }
+        /// <summary>
+        /// ASYNC - Make a request to a path on the API
+        /// </summary>
+        /// <param name="url">Path and query</param>
+        /// <param name="baseUrl">Override the base url, null for the default BaseUrl</param>
+        /// <param name="payload">Payload, can be null. For private API end points, the payload must contain a 'nonce' key with a double value, set to unix timestamp in seconds.
+        /// The encoding of payload is exchange dependant but is typically json.</param>
+        /// <param name="method">Request method or null for default</param>
+        /// <returns>Raw response</returns>
+        public Task<string> MakeRequestAsync(string url, string baseUrl = null, Dictionary<string, object> payload = null, string method = null) => Task.Factory.StartNew(() => MakeRequest(url, baseUrl, payload, method));
 
         /// <summary>
         /// Make a JSON request to an API end point
@@ -272,58 +343,27 @@ namespace ExchangeSharp
         }
 
         /// <summary>
-        /// Get an exchange API given an exchange name (see public constants at top of this file)
+        /// ASYNC - Make a JSON request to an API end point
         /// </summary>
-        /// <param name="exchangeName">Exchange name</param>
-        /// <returns>Exchange API or null if not found</returns>
-        public static IExchangeAPI GetExchangeAPI(string exchangeName)
-        {
-            GetExchangeAPIDictionary().TryGetValue(exchangeName, out IExchangeAPI api);
-            return api;
-        }
-
-        /// <summary>
-        /// Get a dictionary of exchange APIs for all exchanges
-        /// </summary>
-        /// <returns>Dictionary of string exchange name and value exchange api</returns>
-        public static Dictionary<string, IExchangeAPI> GetExchangeAPIDictionary()
-        {
-            Dictionary<string, IExchangeAPI> apis = new Dictionary<string, IExchangeAPI>(StringComparer.OrdinalIgnoreCase);
-            foreach (Type type in typeof(ExchangeAPI).Assembly.GetTypes().Where(type => type.IsSubclassOf(typeof(ExchangeAPI))))
-            {
-                ExchangeAPI api = Activator.CreateInstance(type) as ExchangeAPI;
-                apis[api.Name] = api;
-            }
-            return apis;
-        }
-
-        /// <summary>
-        /// Load API keys from an encrypted file - keys will stay encrypted in memory
-        /// </summary>
-        /// <param name="encryptedFile">Encrypted file to load keys from</param>
-        public virtual void LoadAPIKeys(string encryptedFile)
-        {
-            SecureString[] strings = CryptoUtility.LoadProtectedStringsFromFile(encryptedFile);
-            if (strings.Length != 2)
-            {
-                throw new InvalidOperationException("Encrypted keys file should have a public and private key");
-            }
-            PublicApiKey = strings[0];
-            PrivateApiKey = strings[1];
-        }
-
-        /// <summary>
-        /// Normalize a symbol for use on this exchange
-        /// </summary>
-        /// <param name="symbol">Symbol</param>
-        /// <returns>Normalized symbol</returns>
-        public virtual string NormalizeSymbol(string symbol) { return symbol; }
+        /// <typeparam name="T">Type of object to parse JSON as</typeparam>
+        /// <param name="url">Path and query</param>
+        /// <param name="baseUrl">Override the base url, null for the default BaseUrl</param>
+        /// <param name="payload">Payload, can be null. For private API end points, the payload must contain a 'nonce' key with a double value, set to unix timestamp in seconds.</param>
+        /// <param name="requestMethod">Request method or null for default</param>
+        /// <returns>Result decoded from JSON response</returns>
+        public Task<T> MakeJsonRequestAsync<T>(string url, string baseUrl = null, Dictionary<string, object> payload = null, string requestMethod = null) => Task.Factory.StartNew(() => MakeJsonRequest<T>(url, baseUrl, payload, requestMethod));
 
         /// <summary>
         /// Get exchange symbols
         /// </summary>
         /// <returns>Array of symbols</returns>
         public virtual IReadOnlyCollection<string> GetSymbols() { throw new NotImplementedException(); }
+
+        /// <summary>
+        /// ASYNC - Get exchange symbols
+        /// </summary>
+        /// <returns>Array of symbols</returns>
+        public Task<IReadOnlyCollection<string>> GetSymbolsAsync() => Task.Factory.StartNew(() => GetSymbols());
 
         /// <summary>
         /// Get exchange ticker
@@ -333,10 +373,23 @@ namespace ExchangeSharp
         public virtual ExchangeTicker GetTicker(string symbol) { throw new NotImplementedException(); }
 
         /// <summary>
-        /// Get all tickers
+        /// ASYNC - Get exchange ticker
+        /// </summary>
+        /// <param name="symbol">Symbol to get ticker for</param>
+        /// <returns>Ticker</returns>
+        public Task<ExchangeTicker> GetTickerAsync(string symbol) => Task.Factory.StartNew(() => GetTicker(symbol));
+
+        /// <summary>
+        /// Get all tickers, not all exchanges support this
         /// </summary>
         /// <returns>Key value pair of symbol and tickers array</returns>
         public virtual IReadOnlyCollection<KeyValuePair<string, ExchangeTicker>> GetTickers() { throw new NotImplementedException(); }
+
+        /// <summary>
+        /// ASYNC - Get all tickers, not all exchanges support this
+        /// </summary>
+        /// <returns>Key value pair of symbol and tickers array</returns>
+        public Task<IReadOnlyCollection<KeyValuePair<string, ExchangeTicker>>> GetTickersAsync() => Task.Factory.StartNew(() => GetTickers());
 
         /// <summary>
         /// Get exchange order book
@@ -347,11 +400,26 @@ namespace ExchangeSharp
         public virtual ExchangeOrderBook GetOrderBook(string symbol, int maxCount = 100) { throw new NotImplementedException(); }
 
         /// <summary>
-        /// Get all pending orders for all symbols. Not all exchanges support this. Depending on the exchange, the number of bids and asks will have different counts, typically 50-100.
+        /// ASYNC - Get exchange order book
+        /// </summary>
+        /// <param name="symbol">Symbol to get order book for</param>
+        /// <param name="maxCount">Max count, not all exchanges will honor this parameter</param>
+        /// <returns>Exchange order book or null if failure</returns>
+        public Task<ExchangeOrderBook> GetOrderBookAsync(string symbol, int maxCount = 100) => Task.Factory.StartNew(() => GetOrderBook(symbol, maxCount));
+
+        /// <summary>
+        /// Get exchange order book all symbols. Not all exchanges support this. Depending on the exchange, the number of bids and asks will have different counts, typically 50-100.
         /// </summary>
         /// <param name="maxCount">Max count of bids and asks - not all exchanges will honor this parameter</param>
         /// <returns>Symbol and order books pairs</returns>
         public virtual IReadOnlyCollection<KeyValuePair<string, ExchangeOrderBook>> GetOrderBooks(int maxCount = 100) { throw new NotImplementedException(); }
+
+        /// <summary>
+        /// ASYNC - Get exchange order book all symbols. Not all exchanges support this. Depending on the exchange, the number of bids and asks will have different counts, typically 50-100.
+        /// </summary>
+        /// <param name="maxCount">Max count of bids and asks - not all exchanges will honor this parameter</param>
+        /// <returns>Symbol and order books pairs</returns>
+        public Task<IReadOnlyCollection<KeyValuePair<string, ExchangeOrderBook>>> GetOrderBooksAsync(int maxCount = 100) => Task.Factory.StartNew(() => GetOrderBooks(maxCount));
 
         /// <summary>
         /// Get historical trades for the exchange
@@ -362,6 +430,14 @@ namespace ExchangeSharp
         public virtual IEnumerable<ExchangeTrade> GetHistoricalTrades(string symbol, DateTime? sinceDateTime = null) { throw new NotImplementedException(); }
 
         /// <summary>
+        /// ASYNC - Get historical trades for the exchange
+        /// </summary>
+        /// <param name="symbol">Symbol to get historical data for</param>
+        /// <param name="sinceDateTime">Optional date time to start getting the historical data at, null for the most recent data</param>
+        /// <returns>An enumerator that iterates all historical data, this can take quite a while depending on how far back the sinceDateTime parameter goes</returns>
+        public Task<IEnumerable<ExchangeTrade>> GetHistoricalTradesAsync(string symbol, DateTime? sinceDateTime = null) => Task.Factory.StartNew(() => GetHistoricalTrades(symbol, sinceDateTime));
+
+        /// <summary>
         /// Get recent trades on the exchange
         /// </summary>
         /// <param name="symbol">Symbol to get recent trades for</param>
@@ -369,10 +445,23 @@ namespace ExchangeSharp
         public virtual IEnumerable<ExchangeTrade> GetRecentTrades(string symbol) { return GetHistoricalTrades(symbol, null); }
 
         /// <summary>
+        /// ASYNC - Get recent trades on the exchange
+        /// </summary>
+        /// <param name="symbol">Symbol to get recent trades for</param>
+        /// <returns>An enumerator that loops through all trades</returns>
+        public Task<IEnumerable<ExchangeTrade>> GetRecentTradesAsync(string symbol) => Task.Factory.StartNew(() => GetRecentTrades(symbol));
+
+        /// <summary>
         /// Get amounts available to trade, symbol / amount dictionary
         /// </summary>
         /// <returns>Symbol / amount dictionary</returns>
         public virtual Dictionary<string, decimal> GetAmountsAvailableToTrade() { throw new NotImplementedException(); }
+
+        /// <summary>
+        /// ASYNC - Get amounts available to trade, symbol / amount dictionary
+        /// </summary>
+        /// <returns>Symbol / amount dictionary</returns>
+        public Task<Dictionary<string, decimal>> GetAmountsAvailableToTradeAsync() => Task.Factory.StartNew<Dictionary<string, decimal>>(() => GetAmountsAvailableToTrade());
 
         /// <summary>
         /// Place a limit order
@@ -385,11 +474,28 @@ namespace ExchangeSharp
         public virtual ExchangeOrderResult PlaceOrder(string symbol, decimal amount, decimal price, bool buy) { throw new NotImplementedException(); }
 
         /// <summary>
+        /// ASYNC - Place a limit order
+        /// </summary>
+        /// <param name="symbol">Symbol</param>
+        /// <param name="amount">Amount</param>
+        /// <param name="price">Price</param>
+        /// <param name="buy">True to buy, false to sell</param>
+        /// <returns>Result</returns>
+        public Task<ExchangeOrderResult> PlaceOrderAsync(string symbol, decimal amount, decimal price, bool buy) => Task.Factory.StartNew(() => PlaceOrder(symbol, amount, price, buy));
+
+        /// <summary>
         /// Get order details
         /// </summary>
         /// <param name="orderId">Order id to get details for</param>
         /// <returns>Order details</returns>
         public virtual ExchangeOrderResult GetOrderDetails(string orderId) { throw new NotImplementedException(); }
+
+        /// <summary>
+        /// ASYNC - Get order details
+        /// </summary>
+        /// <param name="orderId">Order id to get details for</param>
+        /// <returns>Order details</returns>
+        public Task<ExchangeOrderResult> GetOrderDetailsAsync(string orderId) => Task.Factory.StartNew(() => GetOrderDetails(orderId));
 
         /// <summary>
         /// Get the details of all open orders
@@ -399,15 +505,23 @@ namespace ExchangeSharp
         public virtual IEnumerable<ExchangeOrderResult> GetOpenOrderDetails(string symbol = null) { throw new NotImplementedException(); }
 
         /// <summary>
+        /// ASYNC - Get the details of all open orders
+        /// </summary>
+        /// <param name="symbol">Symbol to get open orders for or null for all</param>
+        /// <returns>All open order details</returns>
+        public Task<IEnumerable<ExchangeOrderResult>> GetOpenOrderDetailsAsync(string symbol = null) => Task.Factory.StartNew(() => GetOpenOrderDetails());
+
+        /// <summary>
         /// Cancel an order, an exception is thrown if error
         /// </summary>
         /// <param name="orderId">Order id of the order to cancel</param>
         public virtual void CancelOrder(string orderId) { throw new NotImplementedException(); }
 
         /// <summary>
-        /// Gets the name of the exchange
+        /// ASYNC - Cancel an order, an exception is thrown if error
         /// </summary>
-        public virtual string Name { get { return "NullExchange"; } }
+        /// <param name="orderId">Order id of the order to cancel</param>
+        public Task CancelOrderAsync(string orderId) => Task.Factory.StartNew(() => CancelOrder(orderId));
     }
 
     /// <summary>
--- ExchangeSharp/API/Backend/IExchangeAPI.cs
+++ ExchangeSharp/API/Backend/IExchangeAPI.cs
@@ -70,6 +70,17 @@ namespace ExchangeSharp
         string MakeRequest(string url, string baseUrl = null, Dictionary<string, object> payload = null, string method = null);
 
         /// <summary>
+        /// ASYNC - Make a raw request to a path on the API
+        /// </summary>
+        /// <param name="url">Path and query</param>
+        /// <param name="baseUrl">Override the base url, null for the default BaseUrl</param>
+        /// <param name="payload">Payload, can be null. For private API end points, the payload must contain a 'nonce' key with a double value, set to unix timestamp in seconds.
+        /// The encoding of payload is exchange dependant but is typically json.</param>
+        /// <param name="method">Request method or null for default</param>
+        /// <returns>Raw response</returns>
+        Task<string> MakeRequestAsync(string url, string baseUrl = null, Dictionary<string, object> payload = null, string method = null);
+
+        /// <summary>
         /// Make a JSON request to an API end point
         /// </summary>
         /// <typeparam name="T">Type of object to parse JSON as</typeparam>
@@ -81,10 +92,27 @@ namespace ExchangeSharp
         T MakeJsonRequest<T>(string url, string baseUrl = null, Dictionary<string, object> payload = null, string requestMethod = null);
 
         /// <summary>
+        /// ASYNC - Make a JSON request to an API end point
+        /// </summary>
+        /// <typeparam name="T">Type of object to parse JSON as</typeparam>
+        /// <param name="url">Path and query</param>
+        /// <param name="baseUrl">Override the base url, null for the default BaseUrl</param>
+        /// <param name="payload">Payload, can be null. For private API end points, the payload must contain a 'nonce' key with a double value, set to unix timestamp in seconds.</param>
+        /// <param name="requestMethod">Request method or null for default</param>
+        /// <returns>Result decoded from JSON response</returns>
+        Task<T> MakeJsonRequestAsync<T>(string url, string baseUrl = null, Dictionary<string, object> payload = null, string requestMethod = null);
+
+        /// <summary>
         /// Get symbols for the exchange
         /// </summary>
         /// <returns>Symbols</returns>
         IReadOnlyCollection<string> GetSymbols();
+
+        /// <summary>
+        /// ASYNC - Get symbols for the exchange
+        /// </summary>
+        /// <returns>Symbols</returns>
+        Task<IReadOnlyCollection<string>> GetSymbolsAsync();
 
         /// <summary>
         /// Get latest ticker
@@ -94,10 +122,23 @@ namespace ExchangeSharp
         ExchangeTicker GetTicker(string symbol);
 
         /// <summary>
-        /// Get all tickers
+        /// ASYNC - Get latest ticker
+        /// </summary>
+        /// <param name="symbol">Symbol</param>
+        /// <returns>Latest ticker</returns>
+        Task<ExchangeTicker> GetTickerAsync(string symbol);
+
+        /// <summary>
+        /// Get all tickers, not all exchanges support this
         /// </summary>
         /// <returns>Key value pair of symbol and tickers array</returns>
         IReadOnlyCollection<KeyValuePair<string, ExchangeTicker>> GetTickers();
+
+        /// <summary>
+        /// ASYNC - Get all tickers, not all exchanges support this
+        /// </summary>
+        /// <returns>Key value pair of symbol and tickers array</returns>
+        Task<IReadOnlyCollection<KeyValuePair<string, ExchangeTicker>>> GetTickersAsync();
 
         /// <summary>
         /// Get pending orders. Depending on the exchange, the number of bids and asks will have different counts, typically 50-100.
@@ -108,11 +149,26 @@ namespace ExchangeSharp
         ExchangeOrderBook GetOrderBook(string symbol, int maxCount = 100);
 
         /// <summary>
-        /// Get all pending orders for all symbols. Not all exchanges support this. Depending on the exchange, the number of bids and asks will have different counts, typically 50-100.
+        /// ASYNC - Get pending orders. Depending on the exchange, the number of bids and asks will have different counts, typically 50-100.
+        /// </summary>
+        /// <param name="symbol">Symbol</param>
+        /// <param name="maxCount">Max count of bids and asks - not all exchanges will honor this parameter</param>
+        /// <returns>Orders</returns>
+        Task<ExchangeOrderBook> GetOrderBookAsync(string symbol, int maxCount = 100);
+
+        /// <summary>
+        /// Get exchange order book for all symbols. Not all exchanges support this. Depending on the exchange, the number of bids and asks will have different counts, typically 50-100.
         /// </summary>
         /// <param name="maxCount">Max count of bids and asks - not all exchanges will honor this parameter</param>
         /// <returns>Symbol and order books pairs</returns>
         IReadOnlyCollection<KeyValuePair<string, ExchangeOrderBook>> GetOrderBooks(int maxCount = 100);
+
+        /// <summary>
+        /// ASYNC - Get exchange order book for all symbols. Not all exchanges support this. Depending on the exchange, the number of bids and asks will have different counts, typically 50-100.
+        /// </summary>
+        /// <param name="maxCount">Max count of bids and asks - not all exchanges will honor this parameter</param>
+        /// <returns>Symbol and order books pairs</returns>
+        Task<IReadOnlyCollection<KeyValuePair<string, ExchangeOrderBook>>> GetOrderBooksAsync(int maxCount = 100);
 
         /// <summary>
         /// Get historical trades
@@ -123,6 +179,14 @@ namespace ExchangeSharp
         IEnumerable<ExchangeTrade> GetHistoricalTrades(string symbol, DateTime? sinceDateTime = null);
 
         /// <summary>
+        /// ASYNC - Get historical trades
+        /// </summary>
+        /// <param name="symbol">Symbol</param>
+        /// <param name="sinceDateTime">Since date time</param>
+        /// <returns>Trades</returns>
+        Task<IEnumerable<ExchangeTrade>> GetHistoricalTradesAsync(string symbol, DateTime? sinceDateTime = null);
+
+        /// <summary>
         /// Get the latest trades
         /// </summary>
         /// <param name="symbol">Symbol</param>
@@ -130,10 +194,23 @@ namespace ExchangeSharp
         IEnumerable<ExchangeTrade> GetRecentTrades(string symbol);
 
         /// <summary>
+        /// ASYNC - Get the latest trades
+        /// </summary>
+        /// <param name="symbol">Symbol</param>
+        /// <returns>Trades</returns>
+        Task<IEnumerable<ExchangeTrade>> GetRecentTradesAsync(string symbol);
+
+        /// <summary>
         /// Get amounts available to trade
         /// </summary>
         /// <returns>Dictionary of symbols and amounts available to trade</returns>
         Dictionary<string, decimal> GetAmountsAvailableToTrade();
+
+        /// <summary>
+        /// ASYNC - Get amounts available to trade
+        /// </summary>
+        /// <returns>Dictionary of symbols and amounts available to trade</returns>
+        Task<Dictionary<string, decimal>> GetAmountsAvailableToTradeAsync();
 
         /// <summary>
         /// Place a limit order
@@ -146,11 +223,28 @@ namespace ExchangeSharp
         ExchangeOrderResult PlaceOrder(string symbol, decimal amount, decimal price, bool buy);
 
         /// <summary>
+        /// ASYNC - Place a limit order
+        /// </summary>
+        /// <param name="symbol">Symbol, i.e. btcusd</param>
+        /// <param name="amount">Amount to buy or sell</param>
+        /// <param name="price">Price to buy or sell at</param>
+        /// <param name="buy">True to buy, false to sell</param>
+        /// <returns>Order result and message string if any</returns>
+        Task<ExchangeOrderResult> PlaceOrderAsync(string symbol, decimal amount, decimal price, bool buy);
+
+        /// <summary>
         /// Get details of an order
         /// </summary>
         /// <param name="orderId">order id</param>
         /// <returns>Order details</returns>
         ExchangeOrderResult GetOrderDetails(string orderId);
+
+        /// <summary>
+        /// ASYNC - Get details of an order
+        /// </summary>
+        /// <param name="orderId">order id</param>
+        /// <returns>Order details</returns>
+        Task<ExchangeOrderResult> GetOrderDetailsAsync(string orderId);
 
         /// <summary>
         /// Get the details of all open orders
@@ -160,9 +254,22 @@ namespace ExchangeSharp
         IEnumerable<ExchangeOrderResult> GetOpenOrderDetails(string symbol = null);
 
         /// <summary>
+        /// ASYNC - Get the details of all open orders
+        /// </summary>
+        /// <param name="symbol">Symbol to get open orders for or null for all</param>
+        /// <returns>All open order details for the specified symbol</returns>
+        Task<IEnumerable<ExchangeOrderResult>> GetOpenOrderDetailsAsync(string symbol = null);
+
+        /// <summary>
         /// Cancel an order, an exception is thrown if failure
         /// </summary>
         /// <param name="orderId">Order id of the order to cancel</param>
         void CancelOrder(string orderId);
+
+        /// <summary>
+        /// ASYNC - Cancel an order, an exception is thrown if failure
+        /// </summary>
+        /// <param name="orderId">Order id of the order to cancel</param>
+        Task CancelOrderAsync(string orderId);
     }
 }
