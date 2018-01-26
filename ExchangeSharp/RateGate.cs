f739bc9127ef50e58e739666f9eb6be87ca7b6ea
Jeff Johnson <jjxtra@gmail.com>
2018-01-25 20:50:27 -0700

    Switch TimeUnit to TimeSpan

--- ExchangeSharp/RateGate.cs
+++ ExchangeSharp/RateGate.cs
@@ -37,7 +37,10 @@ namespace ExchangeSharp
         // Whether this instance is disposed.
         private bool isDisposed;
 
-        // Callback for the exit timer that exits the semaphore based on exit times in the queue and then sets the timer for the nextexit time.
+        /// <summary>
+        /// Callback for the exit timer that exits the semaphore based on exit times in the queue and then sets the timer for the nextexit time.
+        /// </summary>
+        /// <param name="state">State</param>
         private void ExitTimerCallback(object state)
         {
             // While there are exit times that are passed due still in the queue, exit the semaphore and dequeue the exit time.
@@ -50,22 +53,20 @@ namespace ExchangeSharp
 
             // Try to get the next exit time from the queue and compute the time until the next check should take place. If the 
             // queue is empty, then no exit times will occur until at least one time unit has passed.
-            long timeUntilNextCheck;
+            TimeSpan timeUntilNextCheck;
             if (exitTimes.TryPeek(out exitTime))
             {
-                timeUntilNextCheck = (long)(exitTime - DateTime.UtcNow).TotalMilliseconds;
+                timeUntilNextCheck = (exitTime - DateTime.UtcNow);
             }
             else
             {
-                timeUntilNextCheck = TimeUnitMilliseconds;
+                timeUntilNextCheck = TimeUnit;
             }
 
             // Set the timer.
-            exitTimer.Change(timeUntilNextCheck, -1);
+            exitTimer.Change((long)timeUntilNextCheck.TotalMilliseconds, -1);
         }
 
-
-        // Throws an ObjectDisposedException if this object is disposed.
         private void CheckDisposed()
         {
             if (isDisposed)
@@ -115,14 +116,14 @@ namespace ExchangeSharp
             }
 
             Occurrences = occurrences;
-            TimeUnitMilliseconds = (int)timeUnit.TotalMilliseconds;
+            TimeUnit = timeUnit;
 
             // Create the semaphore, with the number of occurrences as the maximum count.
             semaphore = new SemaphoreSlim(Occurrences, Occurrences);
 
             // Create a timer to exit the semaphore. Use the time unit as the original
             // interval length because that's the earliest we will need to exit the semaphore.
-            exitTimer = new Timer(ExitTimerCallback, null, TimeUnitMilliseconds, -1);
+            exitTimer = new Timer(ExitTimerCallback, null, (long)TimeUnit.TotalMilliseconds, -1);
         }
 
         /// <summary>
@@ -148,7 +149,7 @@ namespace ExchangeSharp
             // and add it to the queue.
             if (entered)
             {
-                var timeToExit = (DateTime.UtcNow.AddMilliseconds(TimeUnitMilliseconds));
+                var timeToExit = DateTime.UtcNow + TimeUnit;
                 exitTimes.Enqueue(timeToExit);
             }
 
@@ -190,8 +191,8 @@ namespace ExchangeSharp
         public int Occurrences { get; private set; }
 
         /// <summary>
-        /// The length of the time unit, in milliseconds.
+        /// The length of the time unit
         /// </summary>
-        public int TimeUnitMilliseconds { get; private set; }
+        public TimeSpan TimeUnit { get; private set; }
     }
 }
