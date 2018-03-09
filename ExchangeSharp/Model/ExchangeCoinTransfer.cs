af43d20449581bed6406e537a47c2c2cb882b4bf
fancycat <lukedude@gmail.com>
2018-03-08 21:15:28 -0800

    Added fields to ExchangeCoinTransfer

--- ExchangeSharp/Model/ExchangeCoinTransfer.cs
+++ ExchangeSharp/Model/ExchangeCoinTransfer.cs
@@ -1,6 +1,29 @@
 ﻿namespace ExchangeSharp
 {
+    using System;
+
     public class ExchangeCoinTransfer
     {
+        public string Address { get; set; }
+
+        public decimal Amounts { get; set; }
+
+        public bool Authorized { get; set; }
+
+        public bool Cancelled { get; set; }
+
+        public string Currency { get; set; }
+
+        public bool InvalidAddress { get; set; }
+
+        public DateTime Opened { get; set; }
+
+        public string PaymentId { get; set; }
+
+        public bool PendingPayment { get; set; }
+
+        public decimal TxCost { get; set; }
+
+        public string TxId { get; set; }
     }
 }
