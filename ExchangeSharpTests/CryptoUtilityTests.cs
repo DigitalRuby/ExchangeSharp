4e500c9cd6fb7e45b9c8124b5060deaf6ce205f7
Steven Thewissen <github@thewissen.io>
2018-03-29 20:10:36 +0200

    Added clamping unit tests

--- ExchangeSharpTests/CryptoUtilityTests.cs
+++ ExchangeSharpTests/CryptoUtilityTests.cs
@@ -35,5 +35,23 @@
             Action a = () => CryptoUtility.RoundDown(1.2345m, -1);
             a.Should().Throw<ArgumentOutOfRangeException>();
         }
+
+        [TestMethod]
+        public void ClampPrice()
+        {
+            CryptoUtility.ClampPrice(0.00000100m, 100000.00000000m, 0.00000100m, 0.05507632m).Should().Be(0.055076m);
+            CryptoUtility.ClampPrice(0.00000010m, 100000.00000000m, 0.00000010m, 0.00052286m).Should().Be(0.0005228m);
+            CryptoUtility.ClampPrice(0.00001000m, 100000.00000000m, 0.00001000m, 0.02525215m).Should().Be(0.025252m);
+            CryptoUtility.ClampPrice(0.00001000m, 100000.00000000m, null, 0.00401212m).Should().Be(0.00401212m);
+        }
+
+        [TestMethod]
+        public void ClampQuantity()
+        {
+            CryptoUtility.ClampQuantity(0.01000000m, 90000000.00000000m, 0.01000000m, 34.55215m).Should().Be(34.55m);
+            CryptoUtility.ClampQuantity(0.00100000m, 90000000.00000000m, 0.00100000m, 941.4192).Should().Be(941.419m);
+            CryptoUtility.ClampQuantity(0.00000100m, 90000000.00000000m, 0.00000100m, 172.94102192m).Should().Be(172.941021m);
+            CryptoUtility.ClampQuantity(0.00010000m, 90000000.00000000m, null, 1837.31935m).Should().Be(1837.31935m);
+        }
     }
 }
