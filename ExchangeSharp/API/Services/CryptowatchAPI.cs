2025f6d980af5f7611be6317a151db6592d2514e
dmitry sazonov <dima.sazonoff3@gmail.com>
2018-04-06 13:46:17 -0400

    test commit

--- ExchangeSharp/API/Services/CryptowatchAPI.cs
+++ ExchangeSharp/API/Services/CryptowatchAPI.cs
@@ -107,5 +107,28 @@ namespace ExchangeSharp
                 };
             }
         }
+
+        public ExchangeOrderBook GetOrderBook(string exchange, string symbol, int maxCount = 100)
+        {
+            ExchangeOrderBook book = new ExchangeOrderBook();
+            JObject obj = MakeJsonRequest<JObject>("/markets/" + exchange.ToLower() + "/" + symbol + "/orderbook");
+            JObject result = (JObject)obj["result"];
+            int count = 0;
+            foreach (JArray array in result["asks"])
+            {
+                if (++count > maxCount)
+                    break;
+                book.Asks.Add(new ExchangeOrderPrice { Amount = (decimal)array[1], Price = (decimal)array[0] });
+            }
+            count = 0;
+            foreach (JArray array in result["bids"])
+            {
+                if (++count > maxCount)
+                    break;
+                book.Bids.Add(new ExchangeOrderPrice { Amount = (decimal)array[1], Price = (decimal)array[0] });
+            }
+            return book;
+        }
+
     }
 }
