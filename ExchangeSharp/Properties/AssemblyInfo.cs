eaabf291488ec49a6ac80d2f17962f0f6176eebe
Jeff Johnson <jjxtra@gmail.com>
2018-05-02 11:00:16 -0600

    Update nuget info

--- ExchangeSharp/Properties/AssemblyInfo.cs
+++ ExchangeSharp/Properties/AssemblyInfo.cs
@@ -6,7 +6,7 @@ using System.Runtime.InteropServices;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("ExchangeSharp")]
-[assembly: AssemblyDescription("ExchangeSharp is a C# API for working with various exchanges for stocks and cryptocurrency. Binance, Bitfinex, Bithumb, Bitstamp, Bittrex, Gemini, GDAX, Kraken and Poloniex are supported. Web sockets are also supported and being enhanced.")]
+[assembly: AssemblyDescription("ExchangeSharp is a C# API for working with various cryptocurrency exchanges. Web sockets are also supported for some exchanges.")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("Digital Ruby, LLC")]
 [assembly: AssemblyProduct("ExchangeSharp")]
