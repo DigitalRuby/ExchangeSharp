04781760123ce7bab468ca6fd54aa715b7848e0c
fancycat <lukedude@gmail.com>
2018-05-23 20:49:56 -0700

    Fix for https://github.com/jjxtra/ExchangeSharp/issues/120 Need to force a refresh of cached GetSymbolsMetadata if no result is found during ClampOrderPrice/Quantity

---
+++ ExchangeSharpTests/MockExchangeAPI.cs
@@ -0,0 +1,32 @@
+﻿namespace ExchangeSharpTests
+{
+    using System.Collections.Generic;
+    using System.Threading.Tasks;
+
+    using ExchangeSharp;
+
+    public class MockExchangeAPI : ExchangeAPI
+    {
+        public int OnGetSymbolsMetadataAsyncCalls;
+
+        public override string BaseUrl { get; set; }
+
+        public override string Name { get; }
+
+        public new ExchangeMarket GetExchangeMarket(string symbol)
+        {
+            return base.GetExchangeMarket(symbol);
+        }
+
+        public void SetExchangeMarkets(IEnumerable<ExchangeMarket> markets)
+        {
+            this.exchangeMarkets = markets;
+        }
+
+        protected override async Task<IEnumerable<ExchangeMarket>> OnGetSymbolsMetadataAsync()
+        {
+            this.OnGetSymbolsMetadataAsyncCalls++;
+            return await Task.Run(() => new List<ExchangeMarket>());
+        }
+    }
+}
