d3cb880f38c343307b6ef66e954579a51efe72dc
vslee <dev-github@vslee.com>
2018-11-06 18:56:51 -0800

    Subscribe to trade stream without symbols (#297)

--- ExchangeSharp/API/Exchanges/ZBcom/ExchangeZBcomAPI.cs
+++ ExchangeSharp/API/Exchanges/ZBcom/ExchangeZBcomAPI.cs
@@ -132,7 +132,11 @@ namespace ExchangeSharp
 
         protected override IWebSocket OnGetTradesWebSocket(Action<KeyValuePair<string, ExchangeTrade>> callback, params string[] marketSymbols)
         {
-            return ConnectWebSocket(string.Empty, (_socket, msg) =>
+			if (marketSymbols == null || marketSymbols.Length == 0)
+			{
+				marketSymbols = GetMarketSymbolsAsync().Sync().ToArray();
+			}
+			return ConnectWebSocket(string.Empty, (_socket, msg) =>
             {
                 JToken token = JToken.Parse(msg.ToStringFromUTF8());
                 if (token["dataType"].ToStringInvariant() == "trades")
