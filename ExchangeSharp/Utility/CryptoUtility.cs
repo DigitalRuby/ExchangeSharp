48bdfe06d236e938c2f1025f68cdcb955d6c86a9
Christopher Tso <christso@gmail.com>
2019-01-03 22:55:11 +1100

    Remove GET request method condition in case of non-standard API

--- ExchangeSharp/Utility/CryptoUtility.cs
+++ ExchangeSharp/Utility/CryptoUtility.cs
@@ -660,7 +660,7 @@ namespace ExchangeSharp
             {
                 request.AddHeader("content-length", "0");
             }
-            else if (!string.IsNullOrEmpty(form) && request.Method != "GET")
+            else if (!string.IsNullOrEmpty(form))
             {
                 byte[] bytes = form.ToBytesUTF8();
                 request.AddHeader("content-length", bytes.Length.ToStringInvariant());
