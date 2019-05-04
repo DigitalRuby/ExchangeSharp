eb27f59f873eded0f3f09f9863ba5e2e130494ed
Drakorle <blackdeal@live.fr>
2019-05-04 19:54:13 +0200

    Update ExchangeBitMEXAPI.cs
    support for intraday Datetime:
    it wasn't possible to get a full day 1min candles list, due to limits.

--- ExchangeSharp/API/Exchanges/BitMEX/ExchangeBitMEXAPI.cs
+++ ExchangeSharp/API/Exchanges/BitMEX/ExchangeBitMEXAPI.cs
@@ -369,11 +369,11 @@ namespace ExchangeSharp
             string url = $"/trade/bucketed?binSize={periodString}&partial=false&symbol={marketSymbol}&reverse=true" + marketSymbol;
             if (startDate != null)
             {
-                url += "&startTime=" + startDate.Value.ToString("yyyy-MM-dd");
+                url += "&startTime=" + startDate.Value.ToString("yyyy-MM-ddTHH:mm:ss");
             }
             if (endDate != null)
             {
-                url += "&endTime=" + endDate.Value.ToString("yyyy-MM-dd");
+                url += "&endTime=" + endDate.Value.ToString("yyyy-MM-ddTHH:mm:ss");
             }
             if (limit != null)
             {
