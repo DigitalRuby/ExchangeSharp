2f6e293398cc9e175da56bbfd373b76762d9e3e4
vslee <dev-github@vslee.com>
2019-11-30 19:10:35 -0800

    Binance Jersey: added exchange support (#499)
    - found another Binance...

---
+++ src/ExchangeSharp/API/Exchanges/BinanceGroup/ExchangeBinanceJerseyAPI.cs
@@ -0,0 +1,27 @@
+/*
+MIT LICENSE
+
+Copyright 2017 Digital Ruby, LLC - http://www.digitalruby.com
+
+Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the "Software"), to deal in the Software without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+*/
+
+using ExchangeSharp.BinanceGroup;
+
+namespace ExchangeSharp
+{
+	public class ExchangeBinanceJerseyAPI : BinanceGroupCommon
+	{
+		public override string BaseUrl { get; set; } = "https://api.binance.je/api/v1";
+		public override string BaseUrlWebSocket { get; set; } = "wss://stream.binance.je:9443";
+		public override string BaseUrlPrivate { get; set; } = "https://api.binance.je/api/v3";
+		public override string WithdrawalUrlPrivate { get; set; } = "https://api.binance.je/wapi/v3";
+		public override string BaseWebUrl { get; set; } = "https://www.binance.je";
+	}
+
+	public partial class ExchangeName { public const string BinanceJersey = "BinanceJersey"; }
+}
