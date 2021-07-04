9dc25abbce4abbcc1c833ef2d8ef52a45ec1149e
adrianbegi <adrianbegi@gmail.com>
2021-07-03 21:19:21 -0700

    working subscription

--- src/ExchangeSharp/API/Exchanges/Bittrex/ExchangeBittrexAPI_WebSocket.cs
+++ src/ExchangeSharp/API/Exchanges/Bittrex/ExchangeBittrexAPI_WebSocket.cs
@@ -27,63 +27,233 @@ using System.Security.Cryptography;
 
 using Newtonsoft.Json;
 using Newtonsoft.Json.Linq;
+using Microsoft.AspNet.SignalR.Client;
+using Newtonsoft.Json.Serialization;
+using Newtonsoft.Json.Converters;
 
 namespace ExchangeSharp
 {
-	public partial class ExchangeBittrexAPI
+	public partial class ExchangeBittrexAPI : ExchangeAPI
 	{
 #if HAS_SIGNALR
+		const string URL = "https://socket-v3.bittrex.com/signalr";
 
-		/// <summary>
-		/// Ideally this would be one instance per exchange instance, but Bittrex simply does not work if you request multiple end points over a single hub connection, sigh...
-		/// </summary>
-		public sealed class BittrexWebSocketManager
+		public string[] channels = new string[] {
+				"heartbeat",
+				"trade_BTC-USD",
+				"order"};
+
+		public async Task<SocketClient> ConnectAndAuthenticate()
 		{
-			const string URL = "https://socket-v3.bittrex.com/signalr";
-
-			public BittrexWebSocketManager()
+			var client = new SocketClient(URL);
+			if (await client.Connect())
 			{
-
+				Console.WriteLine("Connected");
 			}
-
-
-			public async Task<IWebSocket> SubscribeToHeartBeat(Func<string, Task> callback)
+			else
 			{
-				SignalrManager.SocketClient client = new SignalrManager.SocketClient(URL);
-				if (await client.Connect())
-				{
-					Console.WriteLine("Connected");
-				}
-				else
-				{
-					Console.WriteLine("Failed to connect");
-
-				}
+				Console.WriteLine("Failed to connect");
 				return client;
 			}
-		}
 
-		private BittrexWebSocketManager webSocket;
+			if (!string.IsNullOrWhiteSpace(CryptoUtility.ToUnsecureString(PrivateApiKey)))
+			{
+				await Authenticate(client, CryptoUtility.ToUnsecureString(PublicApiKey), CryptoUtility.ToUnsecureString(PrivateApiKey));
+				client.SetAuthExpiringHandler(async () =>
+				{
+					Console.WriteLine("Authentication expiring...");
+					await Authenticate(client, CryptoUtility.ToUnsecureString(PublicApiKey), CryptoUtility.ToUnsecureString(PrivateApiKey));
+				});
+			}
+			else
+			{
+				Console.WriteLine("Authentication skipped because API key was not provided");
+			}
 
-		public static string ReverseMarketNameForWS(string WebSocketFeedMarketName)
-		{
-			var pair = WebSocketFeedMarketName.Split('-');
-			return (pair[1] + '-' + pair[0]).ToUpperInvariant();
+			return client;
 		}
 
 		protected override async Task<IWebSocket> OnGetPositionsWebSocketAsync(Action<ExchangePosition> callback)
 		{
+			var client = await ConnectAndAuthenticate();
+			client.AddMessageHandler<object>("order",
+				msg => callback(ParsePosition(msg))
+				);
+			await Subscribe(client,channels);
 
-			async Task innerCallback(string json)
-			{
-
-				Console.WriteLine(json);
-
-			}
-			return await new BittrexWebSocketManager().SubscribeToHeartBeat(innerCallback);
+			return client;
 		}
 
+		private ExchangePosition ParsePosition(object msg)
+		{
+			throw new NotImplementedException();
+		}
+
+		static async Task Authenticate(SocketClient client, string apiKey, string apiSecret)
+		{
+			var result = await client.Authenticate(apiKey, apiSecret);
+			if (result.Success)
+			{
+				Console.WriteLine("Authenticated");
+			}
+			else
+			{
+				Console.WriteLine($"Authentication failed: {result.ErrorCode}");
+			}
+		}
+
+		static async Task Subscribe(SocketClient client, string[] channels)
+		{
+			client.SetHeartbeatHandler(() => Console.WriteLine("<heartbeat>"));
+
+			var response = await client.Subscribe(channels);
+			for (int i = 0; i < channels.Length; i++)
+			{
+				Console.WriteLine(response[i].Success ? $"{channels[i]}: Success" : $"{channels[i]}: {response[i].ErrorCode}");
+			}
+		}
 #endif
 
+	}
+
+
+	public class SocketResponse
+	{
+		public bool Success { get; set; }
+		public string ErrorCode { get; set; }
+	}
+
+
+	public class SocketClient : IWebSocket
+	{
+		private string _url;
+		private HubConnection _hubConnection;
+		private IHubProxy _hubProxy;
+
+		public event WebSocketConnectionDelegate Connected;
+		public event WebSocketConnectionDelegate Disconnected;
+
+		public TimeSpan ConnectInterval { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
+		public TimeSpan KeepAlive { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
+
+		public SocketClient(string url)
+		{
+			_url = url;
+			_hubConnection = new HubConnection(_url);
+			_hubProxy = _hubConnection.CreateHubProxy("c3");
+		}
+
+		public async Task<bool> Connect()
+		{
+			await _hubConnection.Start();
+			return _hubConnection.State == ConnectionState.Connected;
+		}
+
+		public async Task<SocketResponse> Authenticate(string apiKey, string apiKeySecret)
+		{
+			var timestamp = DateTimeOffset.UtcNow.ToUnixTimeMilliseconds();
+			var randomContent = $"{ Guid.NewGuid() }";
+			var content = string.Join("", timestamp, randomContent);
+			var signedContent = CreateSignature(apiKeySecret, content);
+			var result = await _hubProxy.Invoke<SocketResponse>(
+				"Authenticate",
+				apiKey,
+				timestamp,
+				randomContent,
+				signedContent);
+			return result;
+		}
+
+		public IDisposable AddMessageHandler<Tmessage>(string messageName, Action<Tmessage> handler)
+		{
+			return _hubProxy.On(messageName, message =>
+			{
+				var decoded = DataConverter.Decode<Tmessage>(message);
+				handler(decoded);
+			});
+		}
+
+		public void SetHeartbeatHandler(Action handler)
+		{
+			_hubProxy.On("heartbeat", handler);
+		}
+
+		public void SetAuthExpiringHandler(Action handler)
+		{
+			_hubProxy.On("authenticationExpiring", handler);
+		}
+
+		private static string CreateSignature(string apiSecret, string data)
+		{
+			var hmacSha512 = new HMACSHA512(Encoding.ASCII.GetBytes(apiSecret));
+			var hash = hmacSha512.ComputeHash(Encoding.ASCII.GetBytes(data));
+			return BitConverter.ToString(hash).Replace("-", string.Empty);
+		}
+
+		public async Task<List<SocketResponse>> Subscribe(string[] channels)
+		{
+			try
+			{
+				var result = await _hubProxy.Invoke<List<SocketResponse>>("Subscribe", (object)channels);
+				return result;
+			}
+			catch (Exception e)
+			{
+				Console.WriteLine(e.Message);
+				throw;
+			}
+
+		}
+
+		public Task<bool> SendMessageAsync(object message)
+		{
+			throw new NotImplementedException();
+		}
+
+		public void Dispose()
+		{
+			throw new NotImplementedException();
+		}
+	}
+
+	public static class DataConverter
+	{
+		private static JsonSerializerSettings _jsonSerializerSettings = new JsonSerializerSettings
+		{
+			ContractResolver = new CamelCasePropertyNamesContractResolver(),
+			DateFormatHandling = DateFormatHandling.IsoDateFormat,
+			DateTimeZoneHandling = DateTimeZoneHandling.Utc,
+			FloatParseHandling = FloatParseHandling.Decimal,
+			MissingMemberHandling = MissingMemberHandling.Ignore,
+			NullValueHandling = NullValueHandling.Ignore,
+			Converters = new List<JsonConverter>
+			{
+				new StringEnumConverter(),
+			}
+		};
+
+		public static T Decode<T>(string wireData)
+		{
+			// Step 1: Base64 decode the wire data into a gzip blob
+			byte[] gzipData = Convert.FromBase64String(wireData);
+
+			// Step 2: Decompress gzip blob into JSON
+			string json = null;
+
+			using (var decompressedStream = new MemoryStream())
+			using (var compressedStream = new MemoryStream(gzipData))
+			using (var deflateStream = new DeflateStream(compressedStream, CompressionMode.Decompress))
+			{
+				deflateStream.CopyTo(decompressedStream);
+				decompressedStream.Position = 0;
+				using (var streamReader = new StreamReader(decompressedStream))
+				{
+					json = streamReader.ReadToEnd();
+				}
+			}
+
+			// Step 3: Deserialize the JSON string into a strongly-typed object
+			return JsonConvert.DeserializeObject<T>(json, _jsonSerializerSettings);
+		}
 	}
 }
