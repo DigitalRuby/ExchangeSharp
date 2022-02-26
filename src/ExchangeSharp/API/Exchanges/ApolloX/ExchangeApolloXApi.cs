6232a938933446543d7dc92e7e4c6270e38ccf2b
Victor Lee <dev-github@vslee.com>
2022-02-26 14:37:14 -0800

    add trade stream for ApolloX

---
+++ src/ExchangeSharp/API/Exchanges/ApolloX/ExchangeApolloXApi.cs
@@ -0,0 +1,20 @@
+using ExchangeSharp.BinanceGroup;
+using Newtonsoft.Json.Linq;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExchangeSharp
+{
+	public sealed partial class ExchangeApolloXApi : BinanceGroupCommon
+	{
+		public override string BaseUrl { get; set; } = "https://fapi.apollox.finance";
+		public override string BaseUrlWebSocket { get; set; } = "wss://fstream.apollox.finance";
+
+		public override string BaseUrlApi => $"{BaseUrl}/fapi/v1";
+	}
+
+	public partial class ExchangeName { public const string ApolloX = "ApolloX"; }
+}
